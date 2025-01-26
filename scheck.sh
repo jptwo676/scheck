32e7c50f7a41ac365f22b4b1d4b6a6facfca29a5
jptwo676 <joaobtwo@gmail.com>
2025-01-26 16:37:39 -0300

    add response for no args

--- scheck.sh
+++ scheck.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-file1=$(sha256sum $1 | cut -d ' ' -f1)
-file2=$(sha256sum $2 | cut -d ' ' -f1)
-echo "$file1"
-echo "$file2"
+if [ $# -eq 0 ] 
+then
+  echo "No arguments provided.
+  command example: ./scheck sha1 sha2"
+  exit 1
+fi
+  
 
-[ "$file1" == "$file2" ] && echo "Iguais." || echo "Diferentes."
+sha1=$(sha256sum $1 | cut -d ' ' -f1)
+sha2=$(sha256sum $2 | cut -d ' ' -f1)
+echo "$sha1"
+echo "$sha2"
+
+[ "$sha1" == "$sha2" ] && echo "Iguais." || echo "Diferentes."
 
